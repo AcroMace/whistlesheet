c6c5dd967d65d80e15ad0d8fb4addbbedd45f6dc
Andy <acromace@gmail.com>
2014-07-26 22:15:01 -0400

    All bars are exactly 4 beats now
    (Except the last one)

--- whistlesheet.py
+++ whistlesheet.py
@@ -382,80 +382,98 @@ def convert_octave_to_lilypond(octave):
 # Convert notes and duration to Lilypond notation
 def convert_to_lilypond():
 	print('Converting to Lilypond notation')
-	current_line_duration = 0
+	current_bar_duration = 0
 	lily_notes = open('lilypond.ly', 'w')
 	lily_notes.write('\\version "2.18.2"\n\n')
 	lily_notes.write('\\header {\n\ttitle = "WhistleSheet Alpha"\n}\n\n')
 	lily_notes.write('\\absolute {\n\t\\clef treble\n')
 	for n in notes_duration_list:
-		if current_line_duration > 256:
-			current_line_duration -= 256
-			# lily_notes.write('\t\\bar "|" \n')
 		note = n[0]
 		octave = n[1]
 		length = n[2]
 		octave_char = convert_octave_to_lilypond(octave - OCTAVE + 4)
 		while length > 1:
+			# print "Current bar length is %d" % current_bar_duration
+			# print "Note length is %d" % length
+			carry_over_length = 0
+			if current_bar_duration + length > 64:
+				carry_over_length = length - (64 - current_bar_duration)
+				length -= carry_over_length
+				# print "New note length is %d" % length
+				# print "Carrying over %d" % carry_over_length
+			print ""
 			lily_notes.write('\t' + note)
-			if note == 'r':
-				lily_notes.write('\n')
+			# if note == 'r':
+				# lily_notes.write('\n')
 			if note != 'r':
 				lily_notes.write(octave_char)
+			if length > 64:
+				print "Length is greater than to 64"
+				print "This is an error"
 			if length >= 64:
+				print "This should be a full bar"
 				lily_notes.write('1\n')
-				current_line_duration += 64
+				current_bar_duration += 64
 				length -= 64
 			# elif length >= 56:
 			# 	lily_notes.write('2..\n')
-			# 	current_line_duration += 56
+			# 	current_bar_duration += 56
 			# 	length -= 56
 			elif length >= 48:
 				lily_notes.write('2.\n')
-				current_line_duration += 48
+				current_bar_duration += 48
 				length -= 48
 			elif length >= 32:
 				lily_notes.write('2\n')
-				current_line_duration += 32
+				current_bar_duration += 32
 				length -= 32
 			# elif length >= 28:
 			# 	lily_notes.write('4..\n')
 			# 	length -= 28
 			elif length >= 24:
 				lily_notes.write('4.\n')
-				current_line_duration += 24
+				current_bar_duration += 24
 				length -= 24
 			elif length >= 16:
 				lily_notes.write('4\n')
-				current_line_duration += 16
+				current_bar_duration += 16
 				length -= 16
 			# elif length >= 14:
 			# 	lily_notes.write('8..\n')
 			# 	length -= 14
 			elif length >= 12:
 				lily_notes.write('8.\n')
-				current_line_duration += 12
+				current_bar_duration += 12
 				length -= 12
 			elif length >= 8:
 				lily_notes.write('8\n')
-				current_line_duration += 8
+				current_bar_duration += 8
 				length -= 8
 			# elif length >= 7:
 			# 	lily_notes.write('16..\n')
 			# 	length -= 7
 			elif length >= 6:
 				lily_notes.write('16.\n')
-				current_line_duration += 6
+				current_bar_duration += 6
 				length -= 6
 			elif length >= 4:
 				lily_notes.write('16\n')
-				current_line_duration += 4
+				current_bar_duration += 4
 				length -= 4
 			# elif length >= 2:
 			# 	lily_notes.write('16-.\n')
-			# 	current_line_duration += 2
+			# 	current_bar_duration += 2
 			# 	length -= 2
 			else:
+				print "Length was <4, discarding note"
 				length = 0
+			if carry_over_length > 0:
+				lily_notes.write('~')
+				if length == 0:
+					current_bar_duration = 0
+					lily_notes.write('\t\\bar "|" \n')
+				length += carry_over_length
+				carry_over_length = 0
 	lily_notes.write('\t\\bar "|."\n}')
 	lily_notes.close()
 
