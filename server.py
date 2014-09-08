c085e360f0bee8865e73906cf2c40f79e5d98983
Andy <acromace@gmail.com>
2014-09-08 14:10:08 -0400

    Upload things to /uploads

--- server.py
+++ server.py
@@ -1,6 +1,20 @@
-from flask import Flask, request, render_template, make_response, send_from_directory
+import os
+from flask import (Flask, request, redirect, render_template, make_response,
+	               url_for, send_from_directory)
+from werkzeug import secure_filename
 from whistlesheet import WhistleSheet
+
+OUTPUT_FOLDER = 'output'
+UPLOAD_FOLDER = 'uploads'
+
+
 app = Flask(__name__)
+app.config['OUTPUT_FOLDER'] = OUTPUT_FOLDER
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def is_wav_file(filename):
+	return '.' in filename and filename.rsplit('.', 1)[1] == 'wav'
 
 
 @app.route("/sheetify")
@@ -9,11 +23,25 @@ def get_sheet_music():
 	ws.set_bpm(125)
 	ws.set_octave(5)
 	ws.sheetify()
-	return send_from_directory('output', 'lilypond.pdf')
+	return send_from_directory(app.config['OUTPUT_FOLDER'], 'lilypond.pdf')
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def hello():
+	if request.method == 'POST':
+		file = request.files['file']
+		if file and is_wav_file(file.filename):
+			filename = secure_filename(file.filename)
+			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+			print url_for('uploaded_file',
+				filename=filename)
+			return redirect(url_for('uploaded_file',
+				filename=filename))
 	return render_template('index.html')
+
+@app.route("/uploads/<filename>")
+def uploaded_file(filename):
+	return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
 
 if __name__ == '__main__':
 	app.run()
