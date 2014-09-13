10ff6b19fbdc49acc3ebfbab55c3f8031d0baa1f
Andy <acromace@gmail.com>
2014-09-12 23:24:09 -0400

    Record(.js), POST it, then redirect to the sheet/

--- server.py
+++ server.py
@@ -29,10 +29,17 @@ def is_wav_file(filename):
 	return '.' in filename and filename.rsplit('.', 1)[1] == 'wav'
 
 
+# Sends the PDF given the sheet number
+@app.route("/sheet/<number>")
+def show_results_page(number):
+	return send_from_directory(app.config['OUTPUT_FOLDER'], '%s.pdf' % number)
+
+
 @app.route("/", methods=['GET', 'POST'])
 def home():
 	if request.method == 'POST':
-		file = request.files['file']
+		file = request.files['song']
+		print("Received data: %s" % request.form)
 		if file and is_wav_file(file.filename):
 			filename = str(randint(0, 100000000))
 			file.save(os.path.join(app.config['INPUT_FOLDER'], '%s.wav' % filename))
@@ -40,7 +47,8 @@ def home():
 			ws.set_bpm(125)
 			ws.set_octave(5)
 			ws.sheetify()
-			return send_from_directory(app.config['OUTPUT_FOLDER'], '%s.pdf' % filename)
+			return filename
+		return "Invalid request"
 	return render_template('index.html')
 
 
--- static/js/application.js
+++ static/js/application.js
@@ -1,14 +1,88 @@
 
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
+
+var audioContext = new AudioContext();
+var audioRecorder = null;
+var isRecording = false;
+
+// Exports the recording as WAV and sends it to the server
+function sheetify() {
+    audioRecorder.exportWAV(sendToServer);
+}
+
+// Sends the WAV file to the server
+function sendToServer( blob ) {
+	var fd = new FormData();
+	fd.append('title', 'hello');
+	fd.append('song', blob, 'whistle.wav');
+	$.ajax({
+		url: '',
+		type: 'POST',
+		data: fd,
+		processData: false,
+		contentType: false
+	}).done(receiveFromServer);
+}
+
+// Receives the song data from the server
+function receiveFromServer(data) {
+	window.location.replace('/sheet/' + data);
+}
+
+// Stops recording if recording, starts if not
+function toggleRecording() {
+	if (!audioRecorder) {
+        return;
+	} else if (isRecording) {
+        console.log("Stopping recording");
+        isRecording = false;
+        audioRecorder.stop();
+    } else {
+        console.log("Starting recording");
+        isRecording = true;
+        audioRecorder.clear();
+        audioRecorder.record();
+    }
+}
+
+// Fancy WebAudioAPI magic
+function gotStream(stream) {
+    var inputPoint = audioContext.createGain();
+    var audioInput = audioContext.createMediaStreamSource(stream);
+    audioInput.connect(inputPoint);
+    audioRecorder = new Recorder(inputPoint);
+}
+
+// Called when a user presses the record button
 function record() {
-	console.log("Recording...");
+	toggleRecording();
 };
 
+// Called when a user presses the convert button
 function convert() {
-	console.log("Converting...");
+	sheetify();
 };
+
+// Displays an error message for unsupported browsers
+function displayUnsupportedBrowser() {
+	alert("Your browser is not supported. Please try Chrome or Firefox.");
+}
+
 
 window.onload = function init() {
+	// Button event bindings
 	$('#record-button').click(record);
 	$('#convert-button').click(convert);
-	console.log(document.location.href);
+	// Checks that getUserMedia is supported, raises an error if not
+	if (!navigator.getUserMedia) {
+        navigator.getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+    }
+    if (navigator.getUserMedia) {
+	    navigator.getUserMedia({audio:true}, gotStream, function(e) {
+	        alert('Error getting audio');
+	        console.log(e);
+	    });
+	} else {
+		displayUnsupportedBrowser();
+	};
 };
